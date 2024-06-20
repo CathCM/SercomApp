d373a0cec7e3684be763590d72eea69b28127dca
Cath Cardenas <cathcardenasm@gmail.com>
2024-06-21 00:06:55 +0200

    Models updated: Company and CompanyContract

--- Models/Company.cs
+++ Models/Company.cs
@@ -5,6 +5,7 @@ public class Company
     public int Id { get; set; }
     public string Nif { get; set; }
     public string Name { get; set; }
+    public List<string> Emails { get; set; }
     
     public ICollection<Address> Addresses { get; set; }
     public ICollection<Ticket> Tickets { get; set; }
--- Models/CompanyContract.cs
+++ Models/CompanyContract.cs
@@ -4,12 +4,14 @@ public class CompanyContract
 {
     public int Id { get; set; }
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
+    public DateTime? EndDate { get; set; }
     public decimal Amount { get; set; }
     public bool IsActive { get; set; }
+    public string ServicePeriod { get; set; } // Agregado
     public int CompanyId { get; set; }
     public int BusinessLineId { get; set; }
-    
+
     public Company Company { get; set; }
     public BusinessLine BusinessLine { get; set; }
 }
+
