39d063feedabe374a22be8f7a08f489c553c883d
Cath Cardenas <cathcardenasm@gmail.com>
2024-06-25 22:00:28 +0200

    EmployeeMapperProfile updated: EmployeeWoffuDao to Employee

--- Mapping/EmployeeMapperProfile.cs
+++ Mapping/EmployeeMapperProfile.cs
@@ -9,29 +9,12 @@ public class EmployeeMapperProfile : Profile
     public EmployeeMapperProfile()
     {
         CreateMap<EmployeeWoffuDao.Data, Employee>()
-            // .ForMember(dm => dm.Id, opt => opt.MapFrom(src => src.Id))
-            // .ForMember(dm => dm.Name, opt => opt.MapFrom(src => src.Name))
-            // .ForMember(dm => dm.Abilities, opt => opt.MapFrom(src => src.PokemonAbility.Select(e => new PokemonAbility
-            // {
-            //     Name = e.AbilityName,
-            //     IsHidden = e.IsHidden
-            // }).ToList()))
-            // .ForMember(dm => dm.Stats, opt => opt.MapFrom(src => new List<PokemonStat>
-            // {
-            //     new PokemonStat() { Name = "Hp", BaseStat = src.Hp },
-            //     new PokemonStat() { Name = "Defense", BaseStat = src.Defense },
-            //     new PokemonStat() { Name = "Attack", BaseStat = src.Attack },
-            //     new PokemonStat() { Name = "SpecialAttack", BaseStat = src.SpecialAttack },
-            //     new PokemonStat() { Name = "SpecialDefense", BaseStat = src.SpecialDefense },
-            //     new PokemonStat() { Name = "Speed", BaseStat = src.Speed },
-            // }))
-            // .ForMember(dm => dm.Types, opt => opt.MapFrom(src => src.Types.Select(e => new Types
-            // {
-            //     Name = e.Name
-            // }).ToList()));
             .ForMember(em => em.FirstName, opt => opt.MapFrom(src => src.FirstName))
             .ForMember(em => em.LastName, opt => opt.MapFrom(src => src.LastName))
             .ForMember(em => em.CompanyEmail, opt => opt.MapFrom(src => src.Email))
-            .ForMember(em => em.BirthDate, opt => opt.MapFrom(src => src.Birthday));
+            .ForMember(em => em.BirthDate, opt => opt.MapFrom(src => src.Birthday))
+            .ForMember(em => em.WoffuId, opt => opt.MapFrom(src => src.UserId))
+            .ForMember(em => em.ScheduleId, opt => opt.MapFrom(src => src.ScheduleId))
+            .ForMember(em => em.DepartmentId, opt => opt.MapFrom(src => src.DepartmentId));
     }
 }
